aedafa96bc4517f8e13c709c0a529e9022fa3abc
13744 <quanyufeng0806@gmail.com>
2023-04-13 21:22:05 -0600

    display number in qt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,8 @@
 
 #include <QLabel>
 #include <QGridLayout>
-
+#include <iostream>
+#include <string>
 
 MainWindow::MainWindow(Model& model, QWidget *parent)
     : QMainWindow(parent)
@@ -21,19 +22,28 @@ MainWindow::MainWindow(Model& model, QWidget *parent)
     numberLayout->setSpacing(5);
 
     // Create a 4x4 grid of square labels
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
             QLabel *squareLabel = new QLabel(ui->squareWidget);
             squareLabel->setFrameStyle(QFrame::Box);
+            if(model.displayVector[i][j] != 0)
+            squareLabel->setText(QString::number(model.displayVector[i][j]));
+            QFont font("Arial", 20);
+            squareLabel->setFont(font);
+            squareLabel->setAlignment(Qt::AlignCenter);
             gridLayout->addWidget(squareLabel, i, j);
         }
     }
 
     // create 1*9 grid for input numbers
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < 4; i++){
         QLabel *numberLabel = new QLabel(ui->numberWidget);
         numberLabel->setFrameStyle(QFrame::Box);
         numberLayout->addWidget(numberLabel, 0, i);
+        QFont font("Arial", 20);
+        numberLabel->setFont(font);
+        numberLabel->setAlignment(Qt::AlignCenter);
+        numberLabel->setText(QString::number(i+1));
     }
 
 }
