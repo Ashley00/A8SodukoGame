0b202c21a337fdba831d9306ca75d00e066e79a1
FQY <98041495+Quanyu-Feng@users.noreply.github.com>
2023-04-26 15:42:39 -0600

    Update board.cpp
    update board

--- board.cpp
+++ board.cpp
@@ -1,354 +1,223 @@
-#include "model.h"
+#include "board.h"
+#include "ui_board.h"
 #include <QDebug>
-#include <algorithm>
+#include <QMouseEvent>
 
-/**
- * @brief Model::Model Constructor
- * @param parent
- */
-Model::Model(QObject *parent)
-    : QObject{parent}
+Board::Board(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Board)
 {
+    ui->setupUi(this);
+    ui->lines9x9->setVisible(false);
+    ui->lines4x4->setVisible(false);
+    level = 0;
 
 }
 
-//wjw
-void Model::receivePuzzleInput(int input, int indexJ, int indexI, int level, bool checkall)
+void Board::receiveBoard(int level_, std::vector<std::vector<int>> displayVector)
 {
-    std::vector<int> numInRow;
-    std::vector<int> numInCol;
-    std::vector<int> numInBox4;
+    level = level_;
+    vector = std::vector<std::vector<int>>(level, std::vector<int>(level, 0));
+    showLines();
+    /* Sudoku Board Widget */
+    QGridLayout *gridLayout = new QGridLayout(this);
+    gridLayout->setSpacing(0);
 
-    std::vector<int> possibleNumInRow = generateNumVector(level);
-    std::vector<int> possibleNumInCol = generateNumVector(level);
-    std::vector<int> possibleNumInBox = generateNumVector(level);
+    // Create a 4x4 grid of square labels
+    for(int i = 0; i < level; i++) {
+        QVector<QLabel *> row;
+        for(int j = 0; j < level; j++) {
+            QLabel *squareLabel = new QLabel(this);
+            squareLabel->setFrameStyle(QFrame::Box);
 
-    //loop through indexJ row
-    for(int c = 0; c < level; c++){
-        if(currentVector[indexJ][c] != 0 && c != indexI){
-            numInRow.push_back(currentVector[indexJ][c]);
-            possibleNumInRow.erase(std::remove(
-                                        possibleNumInRow.begin(),
-                                        possibleNumInRow.end(),
-                                        currentVector[indexJ][c]),
-                                        possibleNumInRow.end());
+            if(displayVector[i][j] != 0)
+                squareLabel->setText(QString::number(displayVector[i][j]));
+
+            // disable label with fixed number
+            if(!squareLabel->text().isEmpty()){
+                QPalette palette = squareLabel->palette();
+                palette.setColor(QPalette::Disabled, QPalette::Base, Qt::white); // set transparent for disabled background
+                palette.setColor(QPalette::Disabled, QPalette::WindowText, Qt::black); // set black for disabled text
+                squareLabel->setPalette(palette);
+                squareLabel->setEnabled(false);
+            }
+            QFont font("Arial", 20);
+            squareLabel->setFont(font);
+            squareLabel->setAlignment(Qt::AlignCenter);
+            gridLayout->addWidget(squareLabel, i, j);
+            row.push_back(squareLabel);
+        }
+        cells.push_back(row);
+    }
+    emit(sendCells(cells));
+}
+
+void Board::receiveVector(int i, int j, int input)
+{
+    vector[i][j] = input;
+}
+
+void Board::mousePressEvent(QMouseEvent *event)
+{
+    int x = event->pos().x();
+    int y = event->pos().y();
+    setTransparent();
+    for(int i = 0; i < level; i++){
+        for(int j = 0; j < level; j++){
+            QLabel *currLabel = cells[j][i];
+            int width = currLabel->width();
+            int height = currLabel->height();
+            int labelX = currLabel->x();
+            int labelY = currLabel->y();
+            if((x >= labelX) && (x <= labelX + width) && (y >= labelY) && (y <= labelY + height)){
+                highlightCurrentSquare(j, i);
+                highlightCurrentRow(j);
+                highlightCurrentCol(i);
+                setLightBlue(j, i);
+                emit(sendBoxSelected(j, i, level));
+            }
         }
     }
-    //loop through indexI col
-    for(int r = 0; r < level; r++){
-        if(currentVector[r][indexI] != 0 && r != indexJ){
-            numInCol.push_back(currentVector[r][indexI]);
-            possibleNumInCol.erase(std::remove(
-                                        possibleNumInCol.begin(),
-                                        possibleNumInCol.end(),
-                                        currentVector[r][indexI]),
-                                        possibleNumInCol.end());
+}
+void Board::setLightBlue(int j, int i){
+     cells[j][i]->setStyleSheet("QLabel { background-color : rgba(173, 216, 230, 128);}");
+}
+
+void Board::setDarkBlue(int j, int i){
+     cells[j][i]->setStyleSheet("QLabel { background-color : rgba(46, 90, 154, 0.26);}");
+}
+void Board::setTransparent(){
+    for(int i = 0; i < level; i++){
+        for(int j = 0; j < level; j++){
+             cells[i][j]->setStyleSheet("QLabel { background-color : rgba(0, 0, 0, 0);}");
+//            setRedGreen(i, j);
+            if(vector[i][j] == 1){
+                cells[i][j]->setStyleSheet("QLabel { background-color : rgba(0, 0, 0, 0); color: rgba(0, 165, 11, 1);}");
+            }
+            if(vector[i][j] == 2){
+                cells[i][j]->setStyleSheet("QLabel { background-color : rgba(244, 194, 194, 128); color: red;}");
+            }
         }
     }
-    //loop through the box that contains selected index
-    if(level == 4){
-        numInBox4 = sameNumInBox4(indexJ, indexI);
-        for(int i = 0; i < numInBox4.size(); i++){
-            possibleNumInBox.erase(std::remove(
-                                        possibleNumInBox.begin(),
-                                        possibleNumInBox.end(),
-                                        numInBox4.at(i)),
-                                        possibleNumInBox.end());
-        }
-    }else if(level == 9){
+}
 
+void Board::showLines()
+{
+    switch(level){
+    case 4:
+        ui->lines4x4->setVisible(true);
+        break;
+    case 6:
+        break;
+    case 9:
+        ui->lines9x9->setVisible(true);
+        break;
     }
+}
 
-    //emit send6Vectors(numInRow, possibleNumInRow, numInCol, possibleNumInCol, numInBox4, possibleNumInBox);
+Board::~Board()
+{
+    delete ui;
+}
 
-    std::vector commonNumVec = findSameNumsInVectors(possibleNumInRow, possibleNumInCol, possibleNumInBox);
+void Board::highlightCurrentRow(int indexJ)
+{
+    for(int col = 0; col < level; col++){
+       setDarkBlue(indexJ, col);
+       setRedGreen(indexJ, col);
+    }
+}
 
-    bool inputIsPossible = std::find(commonNumVec.begin(), commonNumVec.end(), input) != commonNumVec.end();
+void Board::highlightCurrentCol(int indexI)
+{
+    for(int row = 0; row < level; row++){
+        setDarkBlue(row, indexI);
+        setRedGreen(row, indexI);
+    }
+}
 
-    if(!checkall)
-        currentVector[indexJ][indexI] = input;
-//    else{
-//        if(indexJ == 3 && indexI == 3 && input == 4)
-//            int a = 0;
+void Board::highlightCurrentSquare(int indexJ, int indexI)
+{
+    switch(level){
+
+    case 4:
+        if((indexJ >= 0 && indexJ <= 1) && (indexI >= 0 && indexI <= 1)){
+            for(int i = 0; i < 2; i++)
+                for(int j = 0; j < 2; j++){
+                    setDarkBlue(i, j);
+                    setRedGreen(i, j);
+                }
+        }else if((indexJ >= 2 && indexJ <= 3) && (indexI >= 0 && indexI <= 1)){
+            for(int i = 2; i < 4; i++)
+                for(int j = 0; j < 2; j++){
+                    setDarkBlue(i, j);
+                    setRedGreen(i, j);
+                }
+        }else if((indexJ >= 0 && indexJ <= 1) && (indexI >= 2 && indexI <= 3)){
+            for(int i = 0; i < 2; i++)
+                for(int j = 2; j < 4; j++){
+                    setDarkBlue(i, j);
+                    setRedGreen(i, j);
+                }
+        }else if((indexJ >= 2 && indexJ <= 3) && (indexI >= 2 && indexI <= 3)){
+            for(int i = 2; i < 4; i++)
+                for(int j = 2; j < 4; j++){
+                    setDarkBlue(i, j);
+                    setRedGreen(i, j);
+                }
+        }
+    break;
+    case 9:
+        int tempJ = 0;
+        int tempI = 0;
+        if((indexJ == 0 || indexJ == 3 || indexJ == 6)){
+            tempJ = indexJ;
+            if(indexI == 0 || indexI == 3 || indexI == 6)
+            tempI = indexI;
+            if(indexI == 1 || indexI == 4 || indexI == 7)
+            tempI = indexI - 1;
+            if(indexI == 2 || indexI == 5 || indexI == 8)
+            tempI = indexI - 2;
+        }
+        if((indexJ == 1 || indexJ == 4 || indexJ == 7)){
+            tempJ = indexJ - 1;
+            if(indexI == 0 || indexI == 3 || indexI == 6)
+            tempI = indexI;
+            if(indexI == 1 || indexI == 4 || indexI == 7)
+            tempI = indexI - 1;
+            if(indexI == 2 || indexI == 5 || indexI == 8)
+            tempI = indexI - 2;
+        }
+        if((indexJ == 2 || indexJ == 5 || indexJ == 8)){
+            tempJ = indexJ - 2;
+            if(indexI == 0 || indexI == 3 || indexI == 6)
+            tempI = indexI;
+            if(indexI == 1 || indexI == 4 || indexI == 7)
+            tempI = indexI - 1;
+            if(indexI == 2 || indexI == 5 || indexI == 8)
+            tempI = indexI - 2;
+        }
+        for(int i = tempI; i < tempI + 3; i++)
+            for(int j = tempJ; j < tempJ + 3; j++){
+                setDarkBlue(j, i);
+                setRedGreen(j, i);
+            }
+    break;
+    } // end switch
+}
+
+
+void Board::setRedGreen(int i, int j){
+    if(vector[i][j] == 1)
+        cells[i][j]->setStyleSheet("QLabel { background-color : rgba(46, 90, 154, 0.26); color: rgba(0, 165, 11, 1);}");
+    if(vector[i][j] == 2)
+        cells[i][j]->setStyleSheet("QLabel { background-color : rgba(46, 90, 154, 0.26); color: red;}");
+}
+
+//void Board::setBackColor(int i, int j){
+//    QPalette curCell = cells[i][j]->palette();
+//    if(curCell.color(QPalette::Window) == QColor(46, 90, 154, 0.26)){
+//        qDebug() << "what";
 //    }
-
-    if(inputIsPossible){
-        //currentVector[indexJ][indexI] = input;
-        if(currentVector == solutionVector){
-            emit sendCorrectInput(input, indexJ, indexI);
-            emit sendWonGame();
-        }
-        emit sendCorrectInput(input, indexJ, indexI);
-    }else{
-        emit sendIncorrectInput(input, indexJ, indexI);
-    }
-}
-
-//wjw
-void Model::get7Vectors(int indexJ, int indexI, int _level){
-    std::vector<int> numInRow;
-    std::vector<int> numInCol;
-    std::vector<int> numInBox4;
-
-    std::vector<int> possibleNumInRow = generateNumVector(level);
-    std::vector<int> possibleNumInCol = generateNumVector(level);
-    std::vector<int> possibleNumInBox = generateNumVector(level);
-
-    //loop through indexJ row
-    for(int c = 0; c < level; c++){
-        if(currentVector[indexJ][c] != 0 && c != indexI){
-            numInRow.push_back(currentVector[indexJ][c]);
-            possibleNumInRow.erase(std::remove(
-                                        possibleNumInRow.begin(),
-                                        possibleNumInRow.end(),
-                                        currentVector[indexJ][c]),
-                                        possibleNumInRow.end());
-        }
-    }
-    //loop through indexI col
-    for(int r = 0; r < level; r++){
-        if(currentVector[r][indexI] != 0 && r != indexJ){
-            numInCol.push_back(currentVector[r][indexI]);
-            possibleNumInCol.erase(std::remove(
-                                        possibleNumInCol.begin(),
-                                        possibleNumInCol.end(),
-                                        currentVector[r][indexI]),
-                                        possibleNumInCol.end());
-        }
-    }
-    //loop through the box that contains selected index
-    if(_level == 4){
-        numInBox4 = sameNumInBox4(indexJ, indexI);
-        for(int i = 0; i < numInBox4.size(); i++){
-            possibleNumInBox.erase(std::remove(
-                                        possibleNumInBox.begin(),
-                                        possibleNumInBox.end(),
-                                        numInBox4.at(i)),
-                                        possibleNumInBox.end());
-        }
-    }else if(_level == 9){
-
-    }
-
-    std::vector commonNumVec = findSameNumsInVectors(possibleNumInRow, possibleNumInCol, possibleNumInBox);
-
-    bool isFixedNum = false;
-    if(displayVector[indexJ][indexI] != 0 || currentVector[indexJ][indexI] != 0){
-        isFixedNum = true;
-    }
-
-    emit sendVectorsAndIndex(indexJ, indexI,
-                             numInRow, possibleNumInRow,
-                             numInCol, possibleNumInCol,
-                             numInBox4, possibleNumInBox,
-                             commonNumVec, isFixedNum);
-
-}
-
-//wjw
-/**
- * @brief Model::generateNumVector
- * Generate a vector that contains 1 to given number
- * @param totalNum
- * @return
- */
-std::vector<int> Model::generateNumVector(int totalNum) {
-    std::vector<int> vec;
-      for (int i = 1; i <= totalNum; i++) {
-        vec.push_back(i);
-      }
-      return vec;
-  }
-
-//wjw
-/**
- * @brief Model::findSameNumsInVectors
- * Helper method that finds common numbers in three vectors
- * @param v1
- * @param v2
- * @return
- */
-std::vector<int> Model::findSameNumsInVectors(std::vector<int> v1, std::vector<int> v2, std::vector<int> v3){
-    std::vector<int> tempVec;
-    std::sort(v1.begin(), v1.end());
-    std::sort(v2.begin(), v2.end());
-    std::sort(v3.begin(), v3.end());
-    std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(tempVec));
-    std::vector<int> sameNumVector;
-    std::set_intersection(tempVec.begin(), tempVec.end(), v3.begin(), v3.end(), std::back_inserter(sameNumVector));
-    return sameNumVector;
-}
-
-//wjw
-/**
- * @brief Model::sameNumInBox4
- * In 4*4 sudoku, get all filled numbers in 2*2 box that contains selected cell
- * @param r
- * @param c
- * @return
- */
-std::vector<int> Model::sameNumInBox4(int r, int c){
-    std::vector<int> resultVec;
-    if((r>=0 && r<2) && (c>=0 && c<2)){
-        for(int x = 0; x < 2; x++){
-            for(int y = 0; y < 2; y++){
-                if(currentVector[x][y] != 0 && x != r && y != c){
-                    resultVec.push_back(currentVector[x][y]);
-                }
-            }
-        }
-    }else if((r>=0 && r<2) && (c>=2 && c<4)){
-        for(int x = 0; x < 2; x++){
-            for(int y = 2; y < 4; y++){
-                if(currentVector[x][y] != 0 && x != r && y != c){
-                    resultVec.push_back(currentVector[x][y]);
-                }
-            }
-        }
-    }else if((r>=2 && r<4) && (c>=0 && c<2)){
-        for(int x = 2; x < 4; x++){
-            for(int y = 0; y < 2; y++){
-                if(currentVector[x][y] != 0 && x != r && y != c){
-                    resultVec.push_back(currentVector[x][y]);
-                }
-            }
-        }
-    }else{
-        for(int x = 2; x < 4; x++){
-            for(int y = 2; y < 4; y++){
-                if(currentVector[x][y] != 0 && x != r && y != c){
-                    resultVec.push_back(currentVector[x][y]);
-                }
-            }
-        }
-    }
-    return resultVec;
-}
-
-/**
- * @brief Model::changeLevel
- * Change the sudoku game level, level can be in 4*4, 6*6, 9*9
- * @param level
- */
-void Model::changeLevel(int level)
-{
-    level = level;
-
-}
-
-/**
- * @brief Model::generator
- * Sudoku game algorithm random generator with answers for 9*9 game
- */
-void Model::generator()
-{
-    // resize both display and solution vectors to be 9*9
-    int gameSize = 9;
-    displayVector.resize(gameSize);
-    solutionVector.resize(gameSize);
-    for(int i = 0; i < gameSize; i++)
-    {
-        displayVector[i].resize(gameSize);
-        solutionVector[i].resize(gameSize);
-    }
-
-    for(int i = 0; i < gameSize; i++)
-    {
-        displayVector[i] = {0,0,0,0,0,0,0,0,0};
-        solutionVector[i] = {0,0,0,0,0,0,0,0,0};
-    }
-}
-
-void Model::generateGame(int level_)
-{
-    level = level_;
-    if(level == 4){
-        displayVector = {{2,1,0,0},
-                         {0,3,2,0},
-                         {0,0,0,4},
-                         {1,0,0,0}};
-        prefixVector = {{2,1,0,0},
-                         {0,3,2,0},
-                         {0,0,0,4},
-                         {1,0,0,0}};
-        solutionVector = {{2,1,4,3},
-                          {4,3,2,1},
-                          {3,2,1,4},
-                          {1,4,3,2}};
-        currentVector = displayVector;
-    }
-
-    if(level == 9){
-        displayVector = {{1,2,0,6,0,8,0,0,0},
-                         {5,8,4,2,3,9,7,0,1},
-                         {0,6,0,1,4,0,0,0,0},
-                         {3,7,0,0,6,1,5,8,0},
-                         {6,9,1,0,8,0,2,7,4},
-                         {4,5,8,7,0,2,0,1,3},
-                         {0,3,0,0,2,4,1,5,0},
-                         {2,0,9,8,5,0,4,3,6},
-                         {0,0,0,3,0,6,0,9,2}
-                        };
-        prefixVector = {{1,2,0,6,0,8,0,0,0},
-                        {5,8,4,2,3,9,7,0,1},
-                        {0,6,0,1,4,0,0,0,0},
-                        {3,7,0,0,6,1,5,8,0},
-                        {6,9,1,0,8,0,2,7,4},
-                        {4,5,8,7,0,2,0,1,3},
-                        {0,3,0,0,2,4,1,5,0},
-                        {2,0,9,8,5,0,4,3,6},
-                        {0,0,0,3,0,6,0,9,2}
-                       };
-        solutionVector = {{1,2,3,6,7,8,9,4,5},
-                          {5,8,4,2,3,9,7,6,1},
-                          {9,6,7,1,4,5,3,2,8},
-                          {3,7,2,4,6,1,5,8,9},
-                          {6,9,1,5,8,3,2,7,4},
-                          {4,5,8,7,9,2,6,1,3},
-                          {8,3,6,9,2,4,1,5,7},
-                          {2,1,9,8,5,7,4,3,6},
-                          {7,4,5,3,1,6,8,9,2}
-                         };
-        currentVector = displayVector;
-    }
-}
-
-/**
- * @brief Model::erase
- * Erase the selected number from displayVector in model
- */
-void Model::receiveErase(int indexI, int indexJ)
-{
-    displayVector[indexI][indexJ] = 0;
-    currentVector[indexI][indexJ] = 0;
-    //qDebug() << "display" << displayVector[indexI][indexJ] << "current" << currentVector[indexI][indexJ];
-}
-
-void Model::receiveInitModel(int level_)
-{
-    // initialize with 4*4 Sudoku game
-    level = level_;
-    generateGame(level);
-    emit(sendDispplayVector(displayVector));
-}
-
-void Model::receiveInitExampleModel(int level_)
-{
-    // initialize with 4*4 Sudoku game
-    level = level_;
-    displayVector = {{2,1,0,0},
-                     {0,3,2,0},
-                     {0,0,0,4},
-                     {1,0,0,0}};
-    prefixVector = {{2,1,0,0},
-                     {0,3,2,0},
-                     {0,0,0,4},
-                     {1,0,0,0}};
-    solutionVector = {{2,1,4,3},
-                      {4,3,2,1},
-                      {3,2,1,4},
-                      {1,4,3,2}};
-    currentVector = displayVector;
-    emit(sendDispplayVector(displayVector));
-}
+//}
 
