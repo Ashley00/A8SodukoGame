3e9327948e7fcdb271cd8f2b1e0dd2c8ce58b432
FQY <98041495+Quanyu-Feng@users.noreply.github.com>
2023-04-26 15:44:52 -0600

    Update board.h
    update board

--- board.h
+++ board.h
@@ -1,62 +1,50 @@
-#ifndef MODEL_H
-#define MODEL_H
+#ifndef BOARD_H
+#define BOARD_H
 
-#include <QObject>
+#include <QWidget>
+#include <QLabel>
+#include <QGridLayout>
+#include <iostream>
+#include <QString>
+#include <QVector>
+#include <QGridLayout>
 
-class Model : public QObject
+namespace Ui {
+class Board;
+}
+
+class Board : public QWidget
 {
     Q_OBJECT
+
 public:
-    explicit Model(QObject *parent = nullptr);
+    explicit Board(QWidget *parent = nullptr);
+    void mousePressEvent(QMouseEvent *);
+    void highlightCurrentRow(int);
+    void highlightCurrentCol(int);
+    void highlightCurrentSquare(int, int);
+    void setTransparent();
+    void setRedGreen(int, int);
+    void setLightBlue(int, int);
+    void setDarkBlue(int, int);
+    void showLines();
+    //void setBackColor(int, int);
 
-    // game level, can be 4*4, 6*6, 9*9
-    int level;
-    // vector of numbers to display
-    std::vector<std::vector<int>> displayVector;
-
-    std::vector<std::vector<int>> prefixVector;
-
-    // vector of the solution
-    std::vector<std::vector<int>> solutionVector;
-    // vector of the current state
-    std::vector<std::vector<int>> currentVector;
-    
-    void generateGame(int);
-
-
-    std::vector<int> generateNumVector(int);
-    std::vector<int> findSameNumsInVectors(std::vector<int>, std::vector<int>, std::vector<int>);
-    std::vector<int> sameNumInBox4(int, int);
-
-
-signals:
-    void sendCorrectInput(int, int, int);
-    void sendIncorrectInput(int, int, int);
-    void sendWonGame();
-    void sendDispplayVector(std::vector<std::vector<int>>);
-
-    //wjw
-    void sendVectorsAndIndex(int, int,
-                             std::vector<int>, std::vector<int>,
-                             std::vector<int>, std::vector<int>,
-                             std::vector<int>, std::vector<int>,
-                             std::vector<int>, bool);
+    ~Board();
 
 public slots:
-    void changeLevel(int level);
-    void generator();
+    void receiveBoard(int, std::vector<std::vector<int>>);
+    void receiveVector(int,int,int);
 
-    //wjw
-    void receivePuzzleInput(int, int, int, int, bool);
-    //wjw
-
-    void receiveErase(int, int);
-    void receiveInitModel(int);
-    void receiveInitExampleModel(int);
-
-    //wjw
-    void get7Vectors(int, int, int);
-
+signals:
+    void sendCells(QVector<QVector<QLabel *>>);
+     void sendBoxSelected(int, int, int);
+private:
+    Ui::Board *ui;
+    QVector<QVector<QLabel *>> cells;
+    QVector<QLabel* > numbers;
+    std::vector<std::vector<int>> vector;
+    int level;
 };
 
-#endif // MODEL_H
+#endif // BOARD_H
